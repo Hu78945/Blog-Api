d4465a4a1a3d6cb7a00da18c18056b7531237290
Hu78945 <behzad789@outlook.com>
2022-05-12 19:35:12 +0500

    Now the jwt token will be delted after it had expired

--- models/refreshToken.js
+++ models/refreshToken.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const refreshTokenSchema = new mongoose.Schema({
   refrehToken: { type: String, required: true },
+  createdAt: { type: Date, expires: 3600, default: Date.now },
 });
 module.exports = mongoose.model("RefreshToekns", refreshTokenSchema);
